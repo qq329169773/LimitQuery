d589a03f3190e6cebb574fd57c9765c18d0cce6e
zhangrui <zhangrui25@jd.com>
2016-06-28 18:21:06 +0800

    first commit

---
+++ src/main/java/oso/com/LimitQuery/Query/App.java
@@ -0,0 +1,13 @@
+package oso.com.LimitQuery.Query;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
